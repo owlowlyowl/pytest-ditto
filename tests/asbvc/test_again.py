2738ea45f125f37f1a8dcbe343d831e58a4fe966
owlowlyowl <95459213+owlowlyowl@users.noreply.github.com>
2023-12-20 14:46:13 +1100

    :art: rename tests

--- tests/asbvc/test_again.py
+++ tests/asbvc/test_again.py
@@ -3,26 +3,26 @@ import pytest
 import ditto
 
 
-def get_data(value):
+def get_data(value: int) -> int:
     return value
 
 
-def func(value):
+def func(value: int) -> int:
     return value + 2
 
 
 @ditto.record("yaml")
-def test_xcv(snapshot):
+def test_cache_input_data(snapshot) -> None:
     data = snapshot(get_data(1))
     assert func(data) == 2
 
 
 @pytest.fixture(scope="function")
-def data(snapshot):
+def data(snapshot) -> int:
     return snapshot(get_data(1))
 
 
 @ditto.record("yaml")
-def test_qqq(snapshot, data):
-    f = func(data)
-    assert f == snapshot(f)
+def test_use_cached_input_data_and_snapshot_result(snapshot, data) -> None:
+    result = func(data)
+    assert result == snapshot(result)
