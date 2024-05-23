5c8a4d8b4fbb94b58979f6712c50d517d4244f36
Lachlan <95459213+owlowlyowl@users.noreply.github.com>
2024-05-23 15:14:15 +1000

    Adding simple namespace for pandas explicit marks

--- ditto/__init__.py
+++ ditto/__init__.py
@@ -1,3 +1,5 @@
+from types import SimpleNamespace
+
 import pytest
 
 from ditto._version import __version__ as version
@@ -9,3 +11,7 @@ record = pytest.mark.record
 yaml = record("yaml")
 json = record("json")
 pickle = record("pkl")
+
+pandas = SimpleNamespace(
+    parquet=record("pandas_parquet"),
+)
