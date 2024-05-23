84b236dea9e7cc5fb8111fca6f5f28e2bc4b5fbe
Lachlan <95459213+owlowlyowl@users.noreply.github.com>
2024-05-23 20:32:49 +1000

    Explicit Marks (#13)
    * Add explicit record markers
    
    Add explicit record markers, e.g., yaml, json, pickle.
    
    * Update test_fixture_record_mark.py
    
    * Adding simple namespace for pandas explicit marks
    
    * `pandas` parquet explicit mark test

--- ditto/__init__.py
+++ ditto/__init__.py
@@ -1,3 +1,5 @@
+from types import SimpleNamespace
+
 import pytest
 
 from ditto._version import __version__ as version
@@ -5,3 +7,11 @@ from ditto.snapshot import Snapshot
 from ditto._unittest import DittoTestCase
 
 record = pytest.mark.record
+
+yaml = record("yaml")
+json = record("json")
+pickle = record("pkl")
+
+pandas = SimpleNamespace(
+    parquet=record("pandas_parquet"),
+)
--- tests/ci/test_fixture_record_mark.py
+++ tests/ci/test_fixture_record_mark.py
@@ -44,3 +44,19 @@ def test_pandas_parquet(snapshot) -> None:
 @ditto.record("json")
 def test_only_one_record_mark_allowed(snapshot) -> None:
     snapshot(1, key="a")
+
+
+@ditto.yaml
+def test_explicit_mark_yaml(snapshot) -> None:
+    key = "xyz"
+    snapshot(77, key=key)
+    assert snapshot.filepath(key).exists()
+    assert snapshot.filepath(key).suffix == ".yaml"
+
+
+@ditto.pandas.parquet
+def test_explicit_mark_pandas_parquet(snapshot) -> None:
+    key = "ijk"
+    snapshot(pd.DataFrame({"a": [44, 77], "qwer": [3, 4]}), key=key)
+    assert snapshot.filepath(key).exists()
+    assert snapshot.filepath(key).suffix == ".parquet"
