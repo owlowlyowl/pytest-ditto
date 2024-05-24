3f6658868c614abb46c1c630dde5415dd6a0f9d3
Lachlan <95459213+owlowlyowl@users.noreply.github.com>
2024-05-24 16:39:46 +1000

    Transition to use src layout (#17)
    * :recycle: src layout
    
    * :white_check_mark: parametrize basic plugin check test

---
+++ src/ditto/_version.py
@@ -0,0 +1,1 @@
+__version__ = '0.0.3.post0.dev3+4dc9895'
--- tests/ci/test_plugins.py
+++ tests/ci/test_plugins.py
@@ -1,6 +1,14 @@
+import pytest
+
 import ditto
 
 
-def test_plugin_load() -> None:
+@pytest.mark.parametrize(
+    "plugin_name",
+    ("pickle", "yaml", "json", "pandas_parquet"),
+)
+def test_plugin_load(plugin_name: str) -> None:
     print(ditto.io.IO_REGISTRY)
     assert ditto.io.IO_REGISTRY
+    assert plugin_name in ditto.io.IO_REGISTRY
+    
