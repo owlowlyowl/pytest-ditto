4b53d1e0740e3d629037e227bc2f8e8faecee7f1
owlowlyowl <95459213+owlowlyowl@users.noreply.github.com>
2024-05-25 18:53:58 +1000

    :adhesive_bandage: pkg name fix

--- src/ditto_pandas/__init__.py
+++ src/ditto_pandas/__init__.py
@@ -4,6 +4,6 @@ from ditto_utils import is_optional_dependency_installed
 if not is_optional_dependency_installed("pandas"):
     _msg = (
         "Required dependencies are not installed. "
-        "Please install using `pip install ditto[pandas]`"
+        "Please install using `pip install pytest-ditto[pandas]`"
     )
     raise Exception(_msg)
