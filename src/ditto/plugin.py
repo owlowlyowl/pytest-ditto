122cf9d9e30f5be5ed5f0ff5aaab5faa25085496
owlowlyowl <95459213+owlowlyowl@users.noreply.github.com>
2024-06-10 12:07:12 +1000

    :recycle: make logic easier to understand

--- src/ditto/plugin.py
+++ src/ditto/plugin.py
@@ -1,37 +1,45 @@
+# from dataclasses import dataclass
+
 import pytest
 
 from ditto import Snapshot
 from ditto import io
-from ditto.exceptions import AdditionalMarkError
+from ditto.exceptions import AdditionalMarkError, DittoMarkHasNoIOType
+
+
+# @dataclass(frozen=True)
+# class Parameters:
+#     io_name: str
+
+
+def _record_mark(node) -> pytest.Mark:
+    marks = list(node.iter_markers(name="record"))
+    if len(marks) > 1:
+        raise AdditionalMarkError()
+    return marks[0]
 
 
 @pytest.fixture
 def snapshot(request) -> Snapshot:
 
-    # TODO: sanitise the below mark parsing
-    io_name = None
-    parameters = {}
-    for mark in request.node.iter_markers(name="record"):
-        if mark.args:
-            if io_name is not None:
-                raise AdditionalMarkError()
-            io_name = mark.args[0]
-
-        if mark.kwargs:
-            parameters.update(mark.kwargs)
-
-    io_name = io_name if io_name is not None else "pkl"
+    # mark = _record_mark(request.node)
+    marks = list(request.node.iter_markers(name="record"))
+    match len(marks):
+        case 0:
+            io_type = io.Pickle
+            parameters = {}
+        case 1:
+            if (io_type := io.get(marks[0].args[0])) is None:
+                raise DittoMarkHasNoIOType()
+            parameters = marks[0].kwargs
+        case _:
+            raise AdditionalMarkError()
 
     # TODO: parameterise the output path?
     path = request.path.parent / ".ditto"
     path.mkdir(exist_ok=True)
 
-    return Snapshot(
-        path=path,
-        group_name=request.node.name,
-        key=parameters.get("key"),
-        io=io.get(io_name, default=io.Pickle),
-    )
+    return Snapshot(path=path, group_name=request.node.name, key={}, io=io_type)
 
 
 def pytest_configure(config):
